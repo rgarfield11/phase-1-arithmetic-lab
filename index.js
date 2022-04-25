6fa5b95b8682befe81e5b5ab528295699304661c
Ross <rossgarfield@gmail.com>
2022-04-25 18:33:33 -0500

    commit tree 26071238a4383e3aead82efec1b3d408e4c96e87

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+// test 1
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+// test 2
+const random = Math.floor(Math.random() * 10) + 1;
+// test 3
+let num3 = 5;
+let num4 = 9;
+let mod = (num4 % num3);
+// test 4
+const max = Math.max(1, 2, 3, 4, 20);
